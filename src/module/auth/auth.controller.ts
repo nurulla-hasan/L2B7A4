684dd19db6d5601f2cda7362cbb644cc162d0c68
nurulla-hasan <nurullahasan.dev@gmail.com>
2026-07-30 12:21:21 +0600

    feat(auth): add refresh token to login response

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -24,6 +24,7 @@ const loginUser = catchAsync(async (req, res) => {
     message: "Login successful",
     data: {
       accessToken: result.accessToken,
+      refreshToken: result.refreshToken,
     },
   });
 });
